729157d2f36cf019561f14a54f7ff091a062b430
pragya-codes <pragya21offc@gmail.com>
2024-02-07 13:58:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+	return (
+		<>
+			<Navbar />
+		</>
+	);
+}
+
+function Navbar() {
+	return (
+		<nav className="container flex items-center justify-between p-2">
+			<h1 className="font-bold">productify</h1>
+			<p>T</p>
+			<p>P</p>
+		</nav>
+	);
+}
+export default App;
